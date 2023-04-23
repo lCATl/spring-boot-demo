f65c0a94c10aa819d9bb5861720a38f8cf67b28d
cat <eric97727@163.com>
2023-04-23 15:45:50 +0800

    init Demo

---
+++ spring-boot-mq-rabbitmq/spring-boot-rabbitmq/producer/src/main/java/com/example/RabbitMQProducerApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @author CatQ
+ */
+@SpringBootApplication
+public class RabbitMQProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQProducerApplication.class);
+    }
+}
